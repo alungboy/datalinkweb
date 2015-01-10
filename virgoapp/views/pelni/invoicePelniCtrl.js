5ffb6d840524e8e417a65c53cf30fa34e2af5842
jethro91 <jethrobhaskara91@gmail.com>
2015-01-10 21:29:38 +0800

    update invoice

--- virgoapp/views/pelni/invoicePelniCtrl.js
+++ virgoapp/views/pelni/invoicePelniCtrl.js
@@ -3,35 +3,39 @@ app.controller('InvoicePelniCtrl', ['$scope', '$rootScope', '$stateParams', '$st
     function($scope, $rootScope, $stateParams, $state) {
 
 
-        $scope.totalPenumpang = [1];
+        $scope.listPenumpang = [{
+            Nama: '',
+            Status: '',
+            Umur: null,
+            Harga: null
+        }];
         $scope.newPemasan = {};
         $scope.newPenumpang = [];
-        $scope.total = 1;
 
-        $scope.lockInvoice = function(){
-            $scope.lockform = true;
-        }
-        $scope.unlockInvoice = function(){
-            $scope.lockform = false;
-        }
-        $scope.addTotal = function() {
-            $scope.totalPenumpang = [1];
-            if ($scope.total > 2 && $scope.total < 11)  {
-                for (var i = 2; $scope.total >= i; i++) {
-                    
-                    $scope.totalPenumpang.push(i);
-                   
-                }
-               
-            } else {
-                
-                 $scope.totalPenumpang = [1];
-            }
+
+        $scope.tambahPng = function() {
+            $scope.listPenumpang.push({
+                Nama: '',
+                Status: '',
+                Umur: null,
+                Harga: null
+            });
         };
 
-        $scope.printInvoice = function(){
+        $scope.rmPng = function(key){
+            $scope.listPenumpang.splice(key, 1);
+        }
+
+        $scope.lockInvoice = function() {
+            $scope.lockform = true;
+        }
+        $scope.unlockInvoice = function() {
+            $scope.lockform = false;
+        }
+    
+        $scope.printInvoice = function() {
             console.log($scope.newPemasan);
-            console.log($scope.newPenumpang);
+            console.log($scope.listPenumpang);
         };
 
     }
